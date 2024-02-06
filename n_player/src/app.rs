7f0436c66757fa5a295a86b90a668cb3c478bc85
enn3 <arturpeshko39@gmail.com>
2024-02-06 02:41:55 +0100

    added fallback font for cjk

--- n_player/src/app.rs
+++ n_player/src/app.rs
@@ -7,6 +7,7 @@ use eframe::egui::{
     Button, Label, Response, ScrollArea, Slider, SliderOrientation, ViewportCommand, Visuals,
     Widget,
 };
+use eframe::epaint::FontFamily;
 use eframe::glow::Context;
 use itertools::Itertools;
 
@@ -27,7 +28,14 @@ pub struct App {
 }
 
 impl App {
-    pub fn new(config: Config, config_path: String, player: QueuePlayer) -> Self {
+    pub fn new(
+        config: Config,
+        config_path: String,
+        player: QueuePlayer,
+        cc: &eframe::CreationContext<'_>,
+    ) -> Self {
+        Self::configure_fonts(&cc.egui_ctx);
+
         let path = config.path().clone().unwrap();
         let paths = fs::read_dir(path).expect("Can't read files in the chosen directory");
         let entries: Vec<DirEntry> = paths.filter_map(|item| item.ok()).collect();
@@ -75,6 +83,59 @@ impl App {
                     .unwrap();
                 self.player.unpause().unwrap();
             }
+        }
+    }
+
+    pub fn configure_fonts(ctx: &egui::Context) -> Option<()> {
+        let font_file = Self::find_cjk_font()?;
+        let font_name = font_file.split('/').last()?.split('.').next()?.to_string();
+        let font_file_bytes = fs::read(font_file).ok()?;
+
+        let font_data = egui::FontData::from_owned(font_file_bytes);
+        let mut font_def = eframe::egui::FontDefinitions::default();
+        font_def.font_data.insert(font_name.to_string(), font_data);
+
+        font_def
+            .families
+            .entry(FontFamily::Proportional)
+            .or_default()
+            .push(font_name);
+
+        ctx.set_fonts(font_def);
+        Some(())
+    }
+
+    fn find_cjk_font() -> Option<String> {
+        #[cfg(unix)]
+        {
+            use std::process::Command;
+            // linux/macOS command: fc-list
+            let output = Command::new("sh").arg("-c").arg("fc-list").output().ok()?;
+            let stdout = std::str::from_utf8(&output.stdout).ok()?;
+            #[cfg(target_os = "macos")]
+            let font_line = stdout
+                .lines()
+                .find(|line| line.contains("Regular") && line.contains("Hiragino Sans GB"))
+                .unwrap_or("/System/Library/Fonts/Hiragino Sans GB.ttc");
+            #[cfg(target_os = "linux")]
+            let font_line = stdout
+                .lines()
+                .find(|line| line.contains("Regular") && line.contains("CJK"))
+                .unwrap_or("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc");
+
+            let font_path = font_line.split(':').next()?.trim();
+            Some(font_path.to_string())
+        }
+        #[cfg(windows)]
+        {
+            let font_file = {
+                // c:/Windows/Fonts/msyh.ttc
+                let mut font_path = PathBuf::from(std::env::var("SystemRoot").ok()?);
+                font_path.push("Fonts");
+                font_path.push("msyh.ttc");
+                font_path.to_str()?.to_string().replace("\\", "/")
+            };
+            Some(font_file)
         }
     }
 }
