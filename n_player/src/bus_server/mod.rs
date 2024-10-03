fd3d9d689a61cc73066aa568efba223d2bee8a0d
enn3 <arturpeshko39@gmail.com>
2024-10-03 19:26:54 +0200

    optimized code for some targets

--- n_player/src/bus_server/mod.rs
+++ n_player/src/bus_server/mod.rs
@@ -39,72 +39,70 @@ pub async fn run<B: BusServer>(
     runner: Arc<RwLock<Runner>>,
     mut tmp: NamedTempFile,
 ) {
-    let mut interval = tokio::time::interval(Duration::from_millis(250));
-    let mut properties = vec![];
-    let mut playback = false;
-    let mut volume = 1.0;
-    let mut index = runner.read().await.index();
-    let path = runner.read().await.path();
-    let queue = runner.read().await.queue();
+    if let Some(server) = server {
+        let mut interval = tokio::time::interval(Duration::from_millis(250));
+        let mut properties = vec![];
+        let mut playback = false;
+        let mut volume = 1.0;
+        let mut index = runner.read().await.index();
+        let path = runner.read().await.path();
+        let queue = runner.read().await.queue();
 
-    loop {
-        interval.tick().await;
-        let guard = runner.read().await;
+        loop {
+            interval.tick().await;
+            let guard = runner.read().await;
 
-        if playback != guard.playback() {
-            playback = guard.playback();
-            properties.push(Property::Playing(playback));
-        }
-        if volume != guard.volume() {
-            volume = guard.volume();
-            properties.push(Property::Volume(volume))
-        }
+            if playback != guard.playback() {
+                playback = guard.playback();
+                properties.push(Property::Playing(playback));
+            }
+            if volume != guard.volume() {
+                volume = guard.volume();
+                properties.push(Property::Volume(volume))
+            }
 
-        if index != guard.index() {
-            index = guard.index();
-            let track_name = &queue[index];
-            let mut path_buf = PathBuf::new();
-            path_buf.push(&path);
-            path_buf.push(track_name);
-            let track = MusicTrack::new(path_buf.to_str().unwrap())
-                .expect("can't get track for currently playing song");
-            let meta = track.get_meta();
-            let image = tokio::task::spawn_blocking(|| get_image(path_buf))
-                .await
-                .unwrap();
-            let image_path = if image.is_empty() {
-                None
-            } else {
-                tmp.rewind().expect("can't rewind tmp file");
-                tmp.write_all(&image)
-                    .expect("can't write image data to tmp file");
-                Some(tmp.path().to_str().unwrap().to_string())
-            };
-            properties.push(Property::Metadata(Metadata {
-                id: String::from("/n_music"),
-                title: Some(if !meta.title.is_empty() {
-                    meta.title
-                } else {
-                    remove_ext(track_name)
-                }),
-                artists: if meta.artist.is_empty() {
+            if index != guard.index() {
+                index = guard.index();
+                let track_name = &queue[index];
+                let mut path_buf = PathBuf::new();
+                path_buf.push(&path);
+                path_buf.push(track_name);
+                let track = MusicTrack::new(path_buf.to_str().unwrap())
+                    .expect("can't get track for currently playing song");
+                let meta = track.get_meta();
+                let image = tokio::task::spawn_blocking(|| get_image(path_buf))
+                    .await
+                    .unwrap();
+                let image_path = if image.is_empty() {
                     None
                 } else {
-                    Some(vec![meta.artist])
-                },
-                length: meta.time.len_secs,
-                image_path,
-            }));
-        }
+                    tmp.rewind().expect("can't rewind tmp file");
+                    tmp.write_all(&image)
+                        .expect("can't write image data to tmp file");
+                    Some(tmp.path().to_str().unwrap().to_string())
+                };
+                properties.push(Property::Metadata(Metadata {
+                    id: String::from("/n_music"),
+                    title: Some(if !meta.title.is_empty() {
+                        meta.title
+                    } else {
+                        remove_ext(track_name)
+                    }),
+                    artists: if meta.artist.is_empty() {
+                        None
+                    } else {
+                        Some(vec![meta.artist])
+                    },
+                    length: meta.time.len_secs,
+                    image_path,
+                }));
+            }
 
-        if !properties.is_empty() {
-            if let Some(server) = &server {
+            if !properties.is_empty() {
                 server
                     .properties_changed(mem::take(&mut properties))
                     .await
                     .unwrap();
-            } else {
-                properties.clear();
             }
         }
     }
