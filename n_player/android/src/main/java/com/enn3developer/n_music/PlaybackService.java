0e1be719e505e99cd7af1abb6a73baef542d2e70
GamerPlayer888 <66787796+GamerPlayer888@users.noreply.github.com>
2024-11-10 19:31:18 +0100

    Add notification permission to app and foreground service

---
+++ n_player/android/src/main/java/com/enn3developer/n_music/PlaybackService.java
@@ -0,0 +1,15 @@
+package com.enn3developer.n_music;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class PlaybackService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) { //TODO
+        return null;
+    }
+}
--- n_player/compile_rust.sh
+++ n_player/compile_rust.sh
@@ -1,1 +1,1 @@
-cargo ndk -t arm64-v8a -o android/src/main/jniLibs/ -p 30 build --package n_player --lib --default-features false
+cargo ndk -t arm64-v8a -o android/src/main/jniLibs/ -p 30 build --package n_player --lib --no-default-features
