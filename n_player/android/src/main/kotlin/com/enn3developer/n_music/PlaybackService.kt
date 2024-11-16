f7deccaf25d71dd85b2bd69b92f144b28356ac74
GamerPlayer888 <66787796+GamerPlayer888@users.noreply.github.com>
2024-11-16 16:10:56 +0100

    Renamed folder and convert PlaybackService to Kotlin

---
+++ n_player/android/src/main/kotlin/com/enn3developer/n_music/PlaybackService.kt
@@ -0,0 +1,11 @@
+package com.enn3developer.n_music
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class PlaybackService : Service() {
+    override fun onBind(intent: Intent): IBinder? { //TODO
+        return null
+    }
+}
